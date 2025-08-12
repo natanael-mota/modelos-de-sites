bdb020c83909c6589ca402754925015b1815ebce
natanael-mota <natanaelmota321@gmail.com>
2025-08-12 02:20:04 -0300

    adiciona animacoes de entrada

---
+++ src/script/animacao.js
@@ -0,0 +1,16 @@
+const observador = new IntersectionObserver((infos) => {
+    console.log(infos)
+    infos.forEach((info) => {
+        if (info.isIntersecting) {
+            info.target.classList.add("visivel")
+        } else {
+            info.target.classList.remove("visivel")
+        }
+    })
+})
+
+const elementos = document.querySelectorAll(".animado")
+
+elementos.forEach((e) => {
+    observador.observe(e)
+})

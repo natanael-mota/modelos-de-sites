da19925a059a6c7529c292c9ae3d6a2e37c1fc79
natanael-mota <natanaelmota321@gmail.com>
2025-08-12 22:21:12 -0300

    fixa os elementos na tela revelando com scroll

--- src/script/animacao.js
+++ src/script/animacao.js
@@ -1,5 +1,4 @@
 const observador = new IntersectionObserver((infos) => {
-    console.log(infos)
     infos.forEach((info) => {
         if (info.isIntersecting) {
             info.target.classList.add("visivel")

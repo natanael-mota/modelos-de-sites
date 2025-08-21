df2837ac50a9779ca84e862aa57b7f5abea2e9a8
natanael-mota <natanaelmota321@gmail.com>
2025-08-21 03:59:32 -0300

    adiciona animacões com scroll nas miniaturas e no titulo

--- portifolio/src/script/animacao.js
+++ portifolio/src/script/animacao.js
@@ -13,3 +13,37 @@ const elementos = document.querySelectorAll(".animado")
 elementos.forEach((e) => {
     observador.observe(e)
 })
+
+const scroller = document.getElementById('background');
+const box = document.getElementById('box');
+const min1 = document.getElementById('min1');
+const min2 = document.getElementById('min2');
+const min3 = document.getElementById('min3');
+const titulo = document.getElementById('slide-titulo')
+
+
+  scroller.addEventListener('scroll', () => {
+    const scrollTop = scroller.scrollTop;
+    const scrollHeight = scroller.scrollHeight - scroller.clientHeight;
+    const progress = scrollTop / scrollHeight;
+
+    if (progress <= 1 && progress > 0.75){
+        removerClass('.miniatura-home', 'mini-atual');
+        min3.classList.add('mini-atual');
+        titulo.style.transform = `translatey(-184px)`;
+    } else if (progress <= 0.75 && progress > 0.25) {
+        removerClass('.miniatura-home', 'mini-atual');
+        min2.classList.add('mini-atual');
+        titulo.style.transform = `translatey(-92px)`;
+    } else {
+        removerClass('.miniatura-home', 'mini-atual');
+        min1.classList.add('mini-atual');
+        titulo.style.transform = `translatey(0)`;
+    }
+  });
+
+  function removerClass(alvo, classe){
+    document.querySelectorAll(alvo).forEach((mini) =>{
+        mini.classList.remove(classe);
+    });
+  }

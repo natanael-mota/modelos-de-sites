c0eaddd3237205607d3cd8f11b9f3baafc42b075
natanael-mota <natanaelmota321@gmail.com>
2025-09-03 23:02:21 -0300

    adiciona efeito hover ao menu

--- portifolio/src/script/menu.js
+++ portifolio/src/script/menu.js
@@ -1,6 +1,10 @@
 const btnMenu = document.querySelector('#botao-menu');
 const menu = document.querySelector('#popup-menu');
 const btnFecharMenu = document.querySelector('#fechar-menu');
+const menuProjetos = document.querySelector('#menu-projetos');
+const menuSobre = document.querySelector('#menu-sobre');
+const menuContatos = document.querySelector('#menu-contatos');
+const conteudoMenu = document.querySelector('#container-conteudo-menu');
 
 btnMenu.addEventListener('click', function (){
     menu.classList.add('menu-aberto');
@@ -8,4 +12,16 @@ btnMenu.addEventListener('click', function (){
 
 btnFecharMenu.addEventListener('click', function (){
     menu.classList.remove('menu-aberto');
+});
+
+menuProjetos.addEventListener('mouseover', function(){
+    conteudoMenu.style.transform = `translatey(0)`;
+});
+
+menuSobre.addEventListener('mouseover', function(){
+    conteudoMenu.style.transform = `translatey(-100vh)`;
+});
+
+menuContatos.addEventListener('mouseover', function(){
+    conteudoMenu.style.transform = `translatey(-200vh)`;
 });
